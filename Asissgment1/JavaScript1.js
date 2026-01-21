87458a3acf18c7d850439a2f01f858fac0fdd1a4
Joshua <2404067@chester.ac.uk>
2026-01-21 17:34:02 +0000

    some javascript tests

--- Asissgment1/JavaScript1.js
+++ Asissgment1/JavaScript1.js
@@ -1,6 +1,6 @@
 ﻿let image = document.getElementById('image')
 let images = ['hue1.png', 'hue1.jpg']
 setInterval(function(){
-	let random = Math.floor(Math.random() * images.length)
+	let random = Math.floor(Math.random() * 2)
 	image.src = images[random]	
 	}, 800)
