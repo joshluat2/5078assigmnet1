b6faa725bec90eab862f1f31900d04eebff38dea
Joshua <2404067@chester.ac.uk>
2026-01-21 17:25:30 +0000

    adding java script functions

---
+++ Asissgment1/JavaScript1.js
@@ -0,0 +1,7 @@
+﻿let image = document.getElementByID('image')
+let image = ['hue1.png', 'hue1.jpg']
+setInterval(function(){
+	let random = Math.floor(Math.random() * 2)
+	image.src = image[random]	
+	}, 800)
+}
