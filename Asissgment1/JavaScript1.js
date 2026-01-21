2648f386b4a5b3db99a70f69000e040e53533901
Joshua <2404067@chester.ac.uk>
2026-01-21 17:29:21 +0000

    fixed some logic and josh not knowing javascriot issues

--- Asissgment1/JavaScript1.js
+++ Asissgment1/JavaScript1.js
@@ -1,7 +1,6 @@
-﻿let image = document.getElementByID('image')
-let image = ['hue1.png', 'hue1.jpg']
+﻿let image = document.getElementById('image')
+let images = ['hue1.png', 'hue1.jpg']
 setInterval(function(){
 	let random = Math.floor(Math.random() * 2)
-	image.src = hue[random]	
+	image.src = images[random]	
 	}, 800)
-}
