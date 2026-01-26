7e8acd36ae159fae37c9ba25948229ff8faadadd
Joshua <2404067@chester.ac.uk>
2026-01-26 14:50:43 +0000

    made css look nicer

--- Asissgment1/JavaScript1.js
+++ Asissgment1/JavaScript1.js
@@ -1,5 +1,5 @@
 ﻿let image = document.getElementById('image')
-let images = ['hue1.png', 'hue2.jpg']
+let images = ['hue1.jpg', 'hue2']
 setInterval(function(){
 	let random = Math.floor(Math.random() * 2)
 	image.src = images[random]	
