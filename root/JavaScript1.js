87e43c1e5b246b4d40815162d7cc14e96e20d2cf
Joshua <2404067@chester.ac.uk>
2026-02-16 08:22:21 +0000

    everything broke again so fixed it

--- root/JavaScript1.js
+++
@@ -1,7 +0,0 @@
-﻿let image = document.getElementById('image')
-let images = ['hue1.jpg', 'hue2']
-setInterval(function(){
-	let random = Math.floor(Math.random() * 2)
-	image.src = images[random]	
-	}, 800)
-	//HTML, A. (2017). How To Automatically Change An HTML Image Using JavaScript. Stack Overflow. https://stackoverflow.com/questions/42003566/how-to-automatically-change-an-html-image-using-javascript
